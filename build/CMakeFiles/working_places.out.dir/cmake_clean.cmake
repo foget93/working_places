d5b393b99bea4b6d989d5aea7198539599b2d30f
Dmitrii Makarov <dmitriymakarov93@gmail.com>
2022-05-12 09:23:35 +0300

    first commit, add .gitignore

---
+++ build/CMakeFiles/working_places.out.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/working_places.out.dir/main.cpp.o"
+  "working_places.out"
+  "working_places.out.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/working_places.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
